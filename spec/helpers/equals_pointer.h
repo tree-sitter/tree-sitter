84b8addb632784490640da1495fd14c8edf5f84d
Max Brunsfeld <maxbrunsfeld@gmail.com>
2014-02-17 12:45:53 -0800

    Organize spec helper files

---
+++ spec/helpers/equals_pointer.h
@@ -0,0 +1,38 @@
+#ifndef tree_sitter_equals_pointer_h
+#define tree_sitter_equals_pointer_h
+
+#include <string>
+
+namespace snowhouse {
+    using namespace std;
+    
+    template<typename ExpectedType>
+    struct EqualsPointerConstraint : Expression<EqualsPointerConstraint<ExpectedType>>
+    {
+        EqualsPointerConstraint(const ExpectedType& expected) : expected(expected) {}
+        
+        template<typename ActualType>
+        bool operator()(const ActualType& actual) const {
+            return *expected == *actual;
+        }
+        
+        ExpectedType expected;
+    };
+    
+    template<typename ExpectedType>
+    struct Stringizer<EqualsPointerConstraint<ExpectedType>>
+    {
+        static string ToString(const EqualsPointerConstraint<ExpectedType>& constraint) {
+            ostringstream builder;
+            builder << "pointer to " << snowhouse::Stringize(constraint.expected);
+            return builder.str();
+        }
+    };
+    
+    template<typename ExpectedType>
+    inline EqualsPointerConstraint<ExpectedType> EqualsPointer(const ExpectedType& expected) {
+        return EqualsPointerConstraint<ExpectedType>(expected);
+    }
+}
+
+#endif
--- spec/spec_helper.h
+++ spec/spec_helper.h
@@ -2,8 +2,8 @@
 #define __tree_sitter_spec_helper_h__
 
 #include "bandit/bandit.h"
-#include "stream_methods.h"
-#include <iostream>
+#include "helpers/stream_methods.h"
+#include "helpers/equals_pointer.h"
 #include "compiler.h"
 
 using namespace tree_sitter;
@@ -12,36 +12,6 @@ using namespace bandit;
 
 #define START_TEST go_bandit([]() {
 #define END_TEST });
-
-namespace snowhouse {
-    template<typename ExpectedType>
-    struct EqualsPointerConstraint : Expression<EqualsPointerConstraint<ExpectedType>>
-    {
-        EqualsPointerConstraint(const ExpectedType& expected) : expected(expected) {}
-        
-        template<typename ActualType>
-        bool operator()(const ActualType& actual) const {
-            return *expected == *actual;
-        }
-        
-        ExpectedType expected;
-    };
-    
-    template<typename ExpectedType>
-    struct Stringizer<EqualsPointerConstraint<ExpectedType>>
-    {
-        static string ToString(const EqualsPointerConstraint<ExpectedType>& constraint) {
-            ostringstream builder;
-            builder << "pointer to " << snowhouse::Stringize(constraint.expected);
-            return builder.str();
-        }
-    };
-
-    template<typename ExpectedType>
-    inline EqualsPointerConstraint<ExpectedType> EqualsPointer(const ExpectedType& expected) {
-        return EqualsPointerConstraint<ExpectedType>(expected);
-    }
-}
 
 string src_dir();
 
