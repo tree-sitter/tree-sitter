4520d6e1a2ccc923e160c3bab7cb3f20b62e91e7
Max Brunsfeld <maxbrunsfeld@gmail.com>
2014-02-23 19:01:24 -0800

    Remove unnecessary downcast in prepare_grammar::perform

--- src/compiler/prepare_grammar/perform.cpp
+++ src/compiler/prepare_grammar/perform.cpp
@@ -8,8 +8,7 @@ namespace tree_sitter {
     
     namespace prepare_grammar {
         pair<PreparedGrammar, PreparedGrammar> perform(const Grammar &input_grammar) {
-            PreparedGrammar grammar(input_grammar);
-            auto grammars = prepare_grammar::extract_tokens(grammar);
+            auto grammars = prepare_grammar::extract_tokens(input_grammar);
             auto rule_grammar = expand_repeats(grammars.first);
             auto lex_grammar = grammars.second;
             return { rule_grammar, lex_grammar };
