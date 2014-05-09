e8760eee34719439ae827d558d0d982395369d34
Max Brunsfeld <maxbrunsfeld@gmail.com>
2014-05-09 16:32:57 -0700

    Don't check in cpplint

--- script/lint.sh
+++ script/lint.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-externals/cpplint.py \
+CPPLINT=externals/cpplint.py
+CPPLINT_URL=http://google-styleguide.googlecode.com/svn/trunk/cpplint/cpplint.py
+
+if [[ ! -f $CPPLINT ]]; then
+  curl $CPPLINT_URL > $CPPLINT
+  chmod +x $CPPLINT
+fi
+
+$CPPLINT \
   --root=src \
   --linelength=110 \
   --filter=-legal/copyright,-readability/namespace,-whitespace/indent,-whitespace/line_length \
