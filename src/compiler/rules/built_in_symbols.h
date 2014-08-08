41c4e7cd8fd35d8f5736556b2b4be0d27b464964
Max Brunsfeld <maxbrunsfeld@gmail.com>
2014-08-08 08:35:26 -0700

    Fix more namespace formatting

--- src/compiler/rules/built_in_symbols.h
+++ src/compiler/rules/built_in_symbols.h
@@ -5,10 +5,12 @@
 
 namespace tree_sitter {
 namespace rules {
+
 Symbol ERROR();
 Symbol START();
 Symbol END_OF_INPUT();
-}
-}
+
+}  // namespace rules
+}  // namespace tree_sitter
 
 #endif  // COMPILER_RULES_BUILT_IN_SYMBOLS_H_
