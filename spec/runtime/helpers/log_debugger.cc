ae0a7fc97dccf1e0fe73519c859e0f31c57f05ab
Max Brunsfeld <maxbrunsfeld@gmail.com>
2014-10-17 23:05:08 -0700

    Add logging debugger for debugging failing tests

---
+++ spec/runtime/helpers/log_debugger.cc
@@ -0,0 +1,21 @@
+#include "tree_sitter/runtime.h"
+#include <stdio.h>
+
+static void log_debug(void *data, TSDebugType type, const char *msg) {
+  switch (type) {
+    case TSDebugTypeParse:
+      fprintf(stderr, "* %s\n", msg);
+      break;
+    case TSDebugTypeLex:
+      fprintf(stderr, "  %s\n", msg);
+      break;
+  }
+}
+
+TSDebugger log_debugger_make() {
+  TSDebugger result;
+  result.data = NULL;
+  result.debug_fn = log_debug;
+  result.release_fn = NULL;
+  return result;
+}
---
+++ spec/runtime/helpers/log_debugger.h
@@ -0,0 +1,8 @@
+#ifndef HELPERS_LOG_DEBUGGER_H_
+#define HELPERS_LOG_DEBUGGER_H_
+
+#include "tree_sitter/runtime.h"
+
+TSDebugger log_debugger_make();
+
+#endif  // HELPERS_LOG_DEBUGGER_H_
