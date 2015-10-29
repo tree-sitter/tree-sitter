04f3bf0b322cbc754337a1fc9be8a9b5783875e4
Rob Rix <rob.rix@github.com>
2015-10-29 12:45:28 -0400

    Implement ts_language_symbol_count.

--- src/runtime/language.c
+++ src/runtime/language.c
@@ -1,5 +1,9 @@
 #include "runtime/parser.h"
 
+size_t ts_language_symbol_count(const TSLanguage *) {
+  return language->symbol_count;
+}
+
 const char *ts_language_symbol_name(TSLanguage *language, TSSymbol symbol) {
   return language->symbol_names[symbol];
 }
