ab00f1b0dabbf7ad849bb0734b6ffff7dcd87c2a
Timothy Clem <timothy.clem@gmail.com>
2017-01-31 15:03:48 -0800

    Add support for \W and \D negated character classes too

--- spec/compiler/prepare_grammar/parse_regex_spec.cc
+++ spec/compiler/prepare_grammar/parse_regex_spec.cc
@@ -30,7 +30,7 @@ describe("parse_regex", []() {
 
     {
       "character classes",
-      "\\w-\\d-\\s-\\S",
+      "\\w-\\d-\\s-\\W-\\D-\\S",
       seq({
         character({
           'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
@@ -43,7 +43,16 @@ describe("parse_regex", []() {
         character({ '-' }),
         character({ ' ', '\t', '\r', '\n' }),
         character({ '-' }),
-        character({ ' ', '\t', '\r', '\n' }, false)
+        character({
+          'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+          'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+          'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+          'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+          '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '_' }, false),
+        character({ '-' }),
+        character({ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }, false),
+        character({ '-' }),
+        character({ ' ', '\t', '\r', '\n' }, false),
       })
     },
 
