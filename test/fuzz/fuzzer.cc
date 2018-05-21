c56c4affb25a3ea4797e0ff29ad3c81718d8ef9e
Phil Turnbull <philipturnbull@github.com>
2018-05-21 10:48:19 -0700

    Use ts_tree_root_node in fuzz driver

--- test/fuzz/fuzzer.cc
+++ test/fuzz/fuzzer.cc
@@ -17,7 +17,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   ts_parser_halt_on_error(parser, TS_HALT_ON_ERROR);
 
   TSTree *tree = ts_parser_parse_string(parser, NULL, str, size);
-  TSNode root_node = ts_document_root_node(tree);
+  TSNode root_node = ts_tree_root_node(tree);
 
   ts_tree_delete(tree);
   ts_parser_delete(parser);
