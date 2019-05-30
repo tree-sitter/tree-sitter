314f3fa00c5d1b6609b72dc36ea64efc7edf77d1
Max Brunsfeld <maxbrunsfeld@gmail.com>
2019-05-30 12:00:12 -0700

    generate-fixtures: handle language repos that contain multiple grammars
    Refs tree-sitter/tree-sitter-typescript#68

--- script/generate-fixtures.cmd
+++ script/generate-fixtures.cmd
@@ -1,22 +1,13 @@
 @echo off
 
-call:regenerate bash
-call:regenerate c
-call:regenerate cpp
-call:regenerate embedded-template
-call:regenerate go
-call:regenerate html
-call:regenerate javascript
-call:regenerate json
-call:regenerate python
-call:regenerate rust
-exit /B 0
+setlocal EnableDelayedExpansion
+set tree_sitter="%cd%\target\release\tree-sitter"
 
-:regenerate
-setlocal
-set tree_sitter=%cd%\target\release\tree-sitter
-set grammar_dir=test\fixtures\grammars\%~1
-pushd %grammar_dir%
-%tree_sitter% generate src\grammar.json
-popd
+for /f "tokens=*" %%f in ('dir test\fixtures\grammars\grammar.js /b/s') do (
+  pushd "%%f\.."
+  echo Regenerating parser !cd!
+  %tree_sitter% generate src\grammar.json
+  popd
+)
+
 exit /B 0
