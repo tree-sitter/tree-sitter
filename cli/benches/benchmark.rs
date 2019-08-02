8083ae660244856d162b6db455aa5250afa02d8e
Max Brunsfeld <maxbrunsfeld@gmail.com>
2019-08-02 12:01:35 -0700

    benchmark: Parse each sample a configurable number of times
    Refs #343

--- cli/benches/benchmark.rs
+++ cli/benches/benchmark.rs
@@ -14,6 +14,10 @@ lazy_static! {
         env::var("TREE_SITTER_BENCHMARK_LANGUAGE_FILTER").ok();
     static ref EXAMPLE_FILTER: Option<String> =
         env::var("TREE_SITTER_BENCHMARK_EXAMPLE_FILTER").ok();
+    static ref REPETITION_COUNT: usize = env::var("TREE_SITTER_BENCHMARK_REPETITION_COUNT")
+        .map(|s| usize::from_str_radix(&s, 10).unwrap())
+        .unwrap_or(5);
+
     static ref TEST_LOADER: Loader = Loader::new(SCRATCH_DIR.clone());
     static ref EXAMPLE_PATHS_BY_LANGUAGE_DIR: BTreeMap<PathBuf, Vec<PathBuf>> = {
         fn process_dir(result: &mut BTreeMap<PathBuf, Vec<PathBuf>>, dir: &Path) {
@@ -72,6 +76,8 @@ fn main() {
                 continue;
             }
         }
+
+        eprintln!("Benchmarking with {} repetitions", *REPETITION_COUNT);
 
         eprintln!("\nLanguage: {}", language_name);
         parser.set_language(get_language(language_path)).unwrap();
@@ -157,10 +163,12 @@ fn parse(parser: &mut Parser, example_path: &Path, max_path_length: usize) -> us
         .map_err(Error::wrap(|| format!("Failed to read {:?}", example_path)))
         .unwrap();
     let time = Instant::now();
-    let _tree = parser
-        .parse(&source_code, None)
-        .expect("Incompatible language version");
-    let duration = time.elapsed();
+    for _ in 0..*REPETITION_COUNT {
+        parser
+            .parse(&source_code, None)
+            .expect("Incompatible language version");
+    }
+    let duration = time.elapsed() / (*REPETITION_COUNT as u32);
     let duration_ms =
         duration.as_secs() as f64 * 1000.0 + duration.subsec_nanos() as f64 / 1000000.0;
     let speed = (source_code.len() as f64 / duration_ms) as usize;
