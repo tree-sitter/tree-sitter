tree-sitter/tree-sitter

commit 68b089b41eb3a93d844e2c802e6a439d68e1e707

cli: Fix generation of parsers with fields but no aliases
Fixes #419

cli/src/generate/render.rs
@@ -79,7 +79,7 @@ impl Generator {
             self.add_field_sequences();
         }
 
-        if !self.alias_ids.is_empty() {
+        if !self.parse_table.production_infos.is_empty() {
             self.add_alias_sequences();
         }
 
@@ -889,7 +889,7 @@ impl Generator {
         add_line!(self, ".lex_modes = ts_lex_modes,");
         add_line!(self, ".symbol_names = ts_symbol_names,");
 
-        if !self.alias_ids.is_empty() {
+        if !self.parse_table.production_infos.is_empty() {
             add_line!(
                 self,
                 ".alias_sequences = (const TSSymbol *)ts_alias_sequences,"

commit 13c0aa7dbb32e8c831b5023bf0525d8bf4912839

Avoid empty initializer list for ts_alias_sequences
Fixes a bug introduced in 68b089b41eb3a93d84

cli/src/generate/render.rs
@@ -364,6 +364,11 @@ impl Generator {
         indent!(self);
         for (i, production_info) in self.parse_table.production_infos.iter().enumerate() {
             if production_info.alias_sequence.is_empty() {
+                // Work around MSVC's intolerance of empty array initializers by
+                // explicitly zero-initializing the first element.
+                if i == 0 {
+                    add_line!(self, "[0] = {{}},");
+                }
                 continue;
             }
 