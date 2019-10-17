5fda03ecf06265b04f61189646e2b6aef4dbd4a0
Matthew Krupcale <mkrupcale@matthewkrupcale.com>
2019-10-17 13:30:55 -0400

    lib: build: Use Meson build in scripts for C library and bindings
     * .appveyor.yml: Install ninja and meson
     * .gitignore: Ignore /build directory
     * .travis.yml: Install ninja and meson
     * Cargo.lock: Remove cc
     * lib/Cargo.toml: Remove cc build dependency
     * lib/binding_rust/build.rs: Use meson and ninja for Rust binding build
     * lib/binding_{rust,web}/meson.build: Include binding helpers in tree-sitter library
     * lib/meson.build: Add binding sub-directories based on build configuration option
     * lib/src/lib.c: Remove monolithic library source file
     * meson.build: Add some default options
     * meson_options.txt: Add option to select the library binding to build: C, Rust, or web
     * script/build-lib: Use meson and ninja for C library build
     * script/build-wasm: Use meson and ninja for web binding build
     * script/util/emscripten-meson-dockerfile: Dockerfile for building library web bindings using Emscripten, meson, and ninja
     * script/util/meson_wasm_cross.txt: Meson cross-file for Emscripten toolchain

--- lib/binding_rust/build.rs
+++ lib/binding_rust/build.rs
@@ -1,7 +1,6 @@
-extern crate cc;
-
 use std::{env, fs};
 use std::path::{Path, PathBuf};
+use std::process::Command;
 
 fn main() {
     println!("cargo:rerun-if-env-changed=TREE_SITTER_STATIC_ANALYSIS");
@@ -19,11 +18,11 @@ fn main() {
         }
     }
 
-    let mut config = cc::Build::new();
+    let mut c_args = "-Wno-unused-parameter".to_string();
 
     println!("cargo:rerun-if-env-changed=TREE_SITTER_TEST");
     if env::var("TREE_SITTER_TEST").is_ok() {
-        config.define("TREE_SITTER_TEST", "");
+        c_args.push_str("-DTREE_SITTER_TEST");
     }
 
     let src_path = Path::new("src");
@@ -34,14 +33,33 @@ fn main() {
         println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
     }
 
-    config
-        .flag_if_supported("-std=c99")
-        .flag_if_supported("-Wno-unused-parameter")
-        .include(src_path)
-        .include("include")
-        .file(src_path.join("lib.c"))
-        .file(Path::new("binding_rust").join("helper.c"))
-        .compile("tree-sitter");
+    let ts_path = Path::new("..");
+    let build_path = ts_path.join("build").join("rust");
+
+    fs::create_dir_all(&build_path).unwrap();
+    Command::new("meson")
+        .arg(ts_path.to_str().unwrap())
+        .arg(build_path.to_str().unwrap())
+        .arg("--buildtype=release")
+        .arg("-Ddefault_library=static")
+        .arg(&format!("-Dc_args=\"{}\"", c_args))
+        .status()
+        .expect("failed to run meson");
+    Command::new("meson")
+        .arg("configure")
+        .arg(build_path.to_str().unwrap())
+        .arg("-Dlib_binding=rust")
+        .status()
+        .expect("failed to run meson configure");
+    Command::new("ninja")
+        .arg("-C")
+        .arg(build_path.to_str().unwrap())
+        .status()
+        .expect("failed to run ninja");
+
+    println!("cargo:rustc-link-search=native={}", build_path.join("lib")
+             .canonicalize().unwrap().to_str().unwrap());
+    println!("cargo:rustc-link-lib=static=tree-sitter");
 }
 
 fn which(exe_name: impl AsRef<Path>) -> Option<PathBuf> {
