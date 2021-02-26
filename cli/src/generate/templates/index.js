075bf2bd5cdfed6d463ce1f9c22ae7f60226b5d2
Max Brunsfeld <maxbrunsfeld@gmail.com>
2021-02-26 12:45:02 -0800

    In generate, create rust bindings
    Also, migrate node binding files into the same 'bindings' folder.

--- cli/src/generate/templates/index.js
+++ cli/src/generate/templates/index.js
@@ -1,17 +1,19 @@
 try {
-  module.exports = require("./build/Release/tree_sitter_PARSER_NAME_binding");
-} catch (error) {
-  if (error.code !== 'MODULE_NOT_FOUND')
-    throw error
-  else try {
-    module.exports = require("./build/Debug/tree_sitter_PARSER_NAME_binding");
+  module.exports = require("../../build/Release/tree_sitter_PARSER_NAME_binding");
+} catch (error1) {
+  if (error1.code !== 'MODULE_NOT_FOUND') {
+    throw error1;
+  }
+  try {
+    module.exports = require("../../build/Debug/tree_sitter_PARSER_NAME_binding");
   } catch (error2) {
-    if (error2.code === 'MODULE_NOT_FOUND')
-      throw error
-    throw error2
+    if (error2.code !== 'MODULE_NOT_FOUND') {
+      throw error2;
+    }
+    throw error1
   }
 }
 
 try {
-  module.exports.nodeTypeInfo = require("./src/node-types.json");
+  module.exports.nodeTypeInfo = require("../../src/node-types.json");
 } catch (_) {}
