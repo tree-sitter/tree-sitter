f22d62393b239f79565f4654a1fb0df1b4656ee9
Andrew Hlynskyi <ahlincq@gmail.com>
2021-06-23 01:02:07 +0300

    fix(cli): actual Rust binding version in generated Cargo.toml

--- cli/build.rs
+++ cli/build.rs
@@ -9,6 +9,12 @@ fn main() {
     if wasm_files_present() {
         println!("cargo:rustc-cfg={}", "TREE_SITTER_EMBED_WASM_BINDING");
     }
+
+    let rust_binding_version = read_rust_binding_version();
+    println!(
+        "cargo:rustc-env={}={}",
+        "RUST_BINDING_VERSION", rust_binding_version
+    );
 }
 
 fn wasm_files_present() -> bool {
@@ -66,4 +72,15 @@ fn read_git_sha() -> Option<String> {
     }
 
     None
+}
+
+fn read_rust_binding_version() -> String {
+    let path = "../lib/Cargo.toml";
+    let text = fs::read_to_string(path).unwrap();
+    let cargo_toml = toml::from_str::<toml::Value>(text.as_ref()).unwrap();
+    cargo_toml["package"]["version"]
+        .as_str()
+        .unwrap()
+        .trim_matches('"')
+        .to_string()
 }
