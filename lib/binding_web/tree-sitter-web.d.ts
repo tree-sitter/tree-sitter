b851b708c2a8e41b8ae694a842f17a30dbf9f15c
Johannes Rieken <johannes.rieken@gmail.com>
2021-07-09 11:13:45 +0200

    update d.ts file because TreeSitter is exported as function

--- lib/binding_web/tree-sitter-web.d.ts
+++ lib/binding_web/tree-sitter-web.d.ts
@@ -1,171 +1,185 @@
 declare module 'web-tree-sitter' {
-  class Parser {
-    static init(): Promise<void>;
-    delete(): void;
-    parse(input: string | Parser.Input, previousTree?: Parser.Tree, options?: Parser.Options): Parser.Tree;
-    getLanguage(): any;
-    setLanguage(language: any): void;
-    getLogger(): Parser.Logger;
-    setLogger(logFunc: Parser.Logger): void;
-  }
 
-  namespace Parser {
-    export type Options = {
-      includedRanges?: Range[];
-    };
+  declare namespace TreeSitter {
 
-    export type Point = {
-      row: number;
-      column: number;
-    };
-
-    export type Range = {
-      startPosition: Point;
-      endPosition: Point;
-      startIndex: number;
-      endIndex: number;
-    };
-
-    export type Edit = {
-      startIndex: number;
-      oldEndIndex: number;
-      newEndIndex: number;
-      startPosition: Point;
-      oldEndPosition: Point;
-      newEndPosition: Point;
-    };
-
-    export type Logger = (
-      message: string,
-      params: { [param: string]: string },
-      type: "parse" | "lex"
-    ) => void;
-
-    export type Input = (
-      startIndex: number,
-      startPoint?: Point,
-      endIndex?: number,
-    ) => string | null;
-
-    export interface SyntaxNode {
-      id: number;
-      tree: Tree;
-      type: string;
-      text: string;
-      startPosition: Point;
-      endPosition: Point;
-      startIndex: number;
-      endIndex: number;
-      parent: SyntaxNode | null;
-      children: Array<SyntaxNode>;
-      namedChildren: Array<SyntaxNode>;
-      childCount: number;
-      namedChildCount: number;
-      firstChild: SyntaxNode | null;
-      firstNamedChild: SyntaxNode | null;
-      lastChild: SyntaxNode | null;
-      lastNamedChild: SyntaxNode | null;
-      nextSibling: SyntaxNode | null;
-      nextNamedSibling: SyntaxNode | null;
-      previousSibling: SyntaxNode | null;
-      previousNamedSibling: SyntaxNode | null;
-
-      hasChanges(): boolean;
-      hasError(): boolean;
-      equals(other: SyntaxNode): boolean;
-      isMissing(): boolean;
-      isNamed(): boolean;
-      toString(): string;
-      child(index: number): SyntaxNode | null;
-      namedChild(index: number): SyntaxNode | null;
-      childForFieldId(fieldId: number): SyntaxNode | null;
-      childForFieldName(fieldName: string): SyntaxNode | null;
-
-      descendantForIndex(index: number): SyntaxNode;
-      descendantForIndex(startIndex: number, endIndex: number): SyntaxNode;
-      descendantsOfType(type: string | Array<string>, startPosition?: Point, endPosition?: Point): Array<SyntaxNode>;
-      namedDescendantForIndex(index: number): SyntaxNode;
-      namedDescendantForIndex(startIndex: number, endIndex: number): SyntaxNode;
-      descendantForPosition(position: Point): SyntaxNode;
-      descendantForPosition(startPosition: Point, endPosition: Point): SyntaxNode;
-      namedDescendantForPosition(position: Point): SyntaxNode;
-      namedDescendantForPosition(startPosition: Point, endPosition: Point): SyntaxNode;
-
-      walk(): TreeCursor;
-    }
-
-    export interface TreeCursor {
-      nodeType: string;
-      nodeText: string;
-      nodeIsNamed: boolean;
-      startPosition: Point;
-      endPosition: Point;
-      startIndex: number;
-      endIndex: number;
-
-      reset(node: SyntaxNode): void;
+    class Parser {
+      static init(): Promise<void>;
       delete(): void;
-      currentNode(): SyntaxNode;
-      currentFieldId(): number;
-      currentFieldName(): string;
-      gotoParent(): boolean;
-      gotoFirstChild(): boolean;
-      gotoFirstChildForIndex(index: number): boolean;
-      gotoNextSibling(): boolean;
-    }
-
-    export interface Tree {
-      readonly rootNode: SyntaxNode;
-
-      copy(): Tree;
-      delete(): void;
-      edit(delta: Edit): Tree;
-      walk(): TreeCursor;
-      getChangedRanges(other: Tree): Range[];
-      getEditedRange(other: Tree): Range;
+      parse(input: string | Parser.Input, previousTree?: Parser.Tree, options?: Parser.Options): Parser.Tree;
       getLanguage(): any;
+      setLanguage(language: any): void;
+      getLogger(): Parser.Logger;
+      setLogger(logFunc: Parser.Logger): void;
+      setTimeoutMicros(value: number): void;
+      getTimeoutMicros(): number;
     }
 
-    class Language {
-      static load(input: string | Uint8Array): Promise<Language>;
+    namespace Parser {
+      export type Options = {
+        includedRanges?: Range[];
+      };
 
-      readonly version: number;
-      readonly fieldCount: number;
-      readonly nodeTypeCount: number;
+      export type Point = {
+        row: number;
+        column: number;
+      };
 
-      fieldNameForId(fieldId: number): string | null;
-      fieldIdForName(fieldName: string): number | null;
-      idForNodeType(type: string, named: boolean): number;
-      nodeTypeForId(typeId: number): string | null;
-      nodeTypeIsNamed(typeId: number): boolean;
-      nodeTypeIsVisible(typeId: number): boolean;
-      query(source: string): Query;
-    }
+      export type Range = {
+        startPosition: Point;
+        endPosition: Point;
+        startIndex: number;
+        endIndex: number;
+      };
 
-    interface QueryCapture {
-      name: string;
-      node: SyntaxNode;
-    }
+      export type Edit = {
+        startIndex: number;
+        oldEndIndex: number;
+        newEndIndex: number;
+        startPosition: Point;
+        oldEndPosition: Point;
+        newEndPosition: Point;
+      };
 
-    interface QueryMatch {
-      pattern: number;
-      captures: QueryCapture[];
-    }
+      export type Logger = (
+        message: string,
+        params: { [param: string]: string },
+        type: "parse" | "lex"
+      ) => void;
 
-    interface PredicateResult {
-      operator: string;
-      operands: { name: string; type: string }[];
-    }
+      export type Input = (
+        startIndex: number,
+        startPoint?: Point,
+        endIndex?: number,
+      ) => string | null;
 
-    class Query {
-      captureNames: string[];
+      export interface SyntaxNode {
+        id: number;
+        tree: Tree;
+        type: string;
+        text: string;
+        startPosition: Point;
+        endPosition: Point;
+        startIndex: number;
+        endIndex: number;
+        parent: SyntaxNode | null;
+        children: Array<SyntaxNode>;
+        namedChildren: Array<SyntaxNode>;
+        childCount: number;
+        namedChildCount: number;
+        firstChild: SyntaxNode | null;
+        firstNamedChild: SyntaxNode | null;
+        lastChild: SyntaxNode | null;
+        lastNamedChild: SyntaxNode | null;
+        nextSibling: SyntaxNode | null;
+        nextNamedSibling: SyntaxNode | null;
+        previousSibling: SyntaxNode | null;
+        previousNamedSibling: SyntaxNode | null;
 
-      delete(): void;
-      matches(node: SyntaxNode, startPosition?: Point, endPosition?: Point): QueryMatch[];
-      captures(node: SyntaxNode, startPosition?: Point, endPosition?: Point): QueryCapture[];
-      predicatesForPattern(patternIndex: number): PredicateResult[];
+        hasChanges(): boolean;
+        hasError(): boolean;
+        equals(other: SyntaxNode): boolean;
+        isMissing(): boolean;
+        isNamed(): boolean;
+        toString(): string;
+        child(index: number): SyntaxNode | null;
+        namedChild(index: number): SyntaxNode | null;
+        childForFieldId(fieldId: number): SyntaxNode | null;
+        childForFieldName(fieldName: string): SyntaxNode | null;
+
+        descendantForIndex(index: number): SyntaxNode;
+        descendantForIndex(startIndex: number, endIndex: number): SyntaxNode;
+        descendantsOfType(type: string | Array<string>, startPosition?: Point, endPosition?: Point): Array<SyntaxNode>;
+        namedDescendantForIndex(index: number): SyntaxNode;
+        namedDescendantForIndex(startIndex: number, endIndex: number): SyntaxNode;
+        descendantForPosition(position: Point): SyntaxNode;
+        descendantForPosition(startPosition: Point, endPosition: Point): SyntaxNode;
+        namedDescendantForPosition(position: Point): SyntaxNode;
+        namedDescendantForPosition(startPosition: Point, endPosition: Point): SyntaxNode;
+
+        walk(): TreeCursor;
+      }
+
+      export interface TreeCursor {
+        nodeType: string;
+        nodeText: string;
+        nodeIsNamed: boolean;
+        startPosition: Point;
+        endPosition: Point;
+        startIndex: number;
+        endIndex: number;
+
+        reset(node: SyntaxNode): void;
+        delete(): void;
+        currentNode(): SyntaxNode;
+        currentFieldId(): number;
+        currentFieldName(): string;
+        gotoParent(): boolean;
+        gotoFirstChild(): boolean;
+        gotoFirstChildForIndex(index: number): boolean;
+        gotoNextSibling(): boolean;
+      }
+
+      export interface Tree {
+        readonly rootNode: SyntaxNode;
+
+        copy(): Tree;
+        delete(): void;
+        edit(delta: Edit): Tree;
+        walk(): TreeCursor;
+        getChangedRanges(other: Tree): Range[];
+        getEditedRange(other: Tree): Range;
+        getLanguage(): any;
+      }
+
+      class Language {
+        static load(input: string | Uint8Array): Promise<Language>;
+
+        readonly version: number;
+        readonly fieldCount: number;
+        readonly nodeTypeCount: number;
+
+        fieldNameForId(fieldId: number): string | null;
+        fieldIdForName(fieldName: string): number | null;
+        idForNodeType(type: string, named: boolean): number;
+        nodeTypeForId(typeId: number): string | null;
+        nodeTypeIsNamed(typeId: number): boolean;
+        nodeTypeIsVisible(typeId: number): boolean;
+        query(source: string): Query;
+      }
+
+      interface QueryCapture {
+        name: string;
+        node: SyntaxNode;
+      }
+
+      interface QueryMatch {
+        pattern: number;
+        captures: QueryCapture[];
+      }
+
+      interface PredicateResult {
+        operator: string;
+        operands: { name: string; type: string }[];
+      }
+
+      class Query {
+        captureNames: string[];
+
+        delete(): void;
+        matches(node: SyntaxNode, startPosition?: Point, endPosition?: Point): QueryMatch[];
+        captures(node: SyntaxNode, startPosition?: Point, endPosition?: Point): QueryCapture[];
+        predicatesForPattern(patternIndex: number): PredicateResult[];
+      }
     }
   }
 
-  export = Parser
+  interface TreeSitterModule {
+    Parser: typeof TreeSitter.Parser;
+    [key: string]: any;
+  }
+
+  declare function TreeSitter(module?: object): Promise<TreeSitterModule>;
+
+  export = TreeSitter
+
 }
