8df0b8de7e5f627f1c0c1a4fb6e2834791923648
Max Brunsfeld <maxbrunsfeld@gmail.com>
2021-12-30 16:27:02 -0800

    Convert more fixture grammars from JSON to JS

---
+++ test/fixtures/test_grammars/inverted_external_token/grammar.js
@@ -0,0 +1,15 @@
+module.exports = grammar({
+  name: "inverted_external_token",
+
+  externals: $ => [$.line_break],
+
+  extras: $ => [/\s/],
+
+  rules: {
+    program: $ => repeat($.statement),
+    statement: $ => seq($._expression, $.line_break),
+    _expression: $ => choice($.identifier, $.member_expression),
+    member_expression: $ => prec.left(seq($._expression, ".", $.identifier)),
+    identifier: $ => /[a-z]+/,
+  },
+});
