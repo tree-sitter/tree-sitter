27dc1dd0a5ca0a44680ee68f62aca8cd8c19ec80
Alex Pinkus <alex.pinkus@gmail.com>
2022-01-15 14:01:40 -0800

    Add Kotlin, Swift, and Elixir to benchmarks
    Because the Swift grammar generates its `grammar.json` from source every
    time, this also modifies the `generate-fixtures` scripts so that they
    attempt an `npm install` when they need to.
    
    This change does not add these repositories into `corpus_test` because
    `elixir` seems to have a spurious failure there and `swift` runs forever
    without terminating.

--- script/fetch-fixtures.cmd
+++ script/fetch-fixtures.cmd
@@ -1,32 +1,36 @@
 @echo off
 
-call:fetch_grammar bash              master
-call:fetch_grammar c                 master
-call:fetch_grammar cpp               master
-call:fetch_grammar embedded-template master
-call:fetch_grammar go                master
-call:fetch_grammar html              master
-call:fetch_grammar java              master
-call:fetch_grammar javascript        partial-order-precedences
-call:fetch_grammar jsdoc             master
-call:fetch_grammar json              master
-call:fetch_grammar php               master
-call:fetch_grammar python            master
-call:fetch_grammar ruby              master
-call:fetch_grammar rust              master
-call:fetch_grammar typescript        master
+call:fetch_grammar tree-sitter bash              master
+call:fetch_grammar tree-sitter c                 master
+call:fetch_grammar tree-sitter cpp               master
+call:fetch_grammar tree-sitter embedded-template master
+call:fetch_grammar tree-sitter go                master
+call:fetch_grammar tree-sitter html              master
+call:fetch_grammar tree-sitter java              master
+call:fetch_grammar tree-sitter javascript        partial-order-precedences
+call:fetch_grammar tree-sitter jsdoc             master
+call:fetch_grammar tree-sitter json              master
+call:fetch_grammar tree-sitter php               master
+call:fetch_grammar tree-sitter python            master
+call:fetch_grammar tree-sitter ruby              master
+call:fetch_grammar tree-sitter rust              master
+call:fetch_grammar tree-sitter typescript        master
+call:fetch_grammar elixir-lang elixir            de20391afe5cb03ef1e8a8e43167e7b58cc52869
+call:fetch_grammar fwcd        kotlin            0.2.11
+call:fetch_grammar alex-pinkus swift             0.1.1-with-static-grammar.json
 exit /B 0
 
 :fetch_grammar
 setlocal
-set grammar_dir=test\fixtures\grammars\%~1
-set grammar_url=https://github.com/tree-sitter/tree-sitter-%~1
-set grammar_branch=%~2
+set organization=%~1
+set grammar_dir=test\fixtures\grammars\%~2
+set grammar_url=https://github.com/%organization%/tree-sitter-%~2
+set grammar_ref=%~3
 @if not exist %grammar_dir% (
   git clone %grammar_url% %grammar_dir% --depth=1
 )
 pushd %grammar_dir%
-git fetch origin %2 --depth=1
+git fetch origin %grammar_ref% --depth=1
 git reset --hard FETCH_HEAD
 popd
 exit /B 0
