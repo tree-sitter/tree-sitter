0e112a76027338a73663978e4d7e45c82b06e23e
Andrew Hlynskyi <ahlincq@gmail.com>
2023-01-15 22:23:15 +0200

    ci: fix parser generation issues due to nextest parallelism

--- script/generate-fixtures.cmd
+++ script/generate-fixtures.cmd
@@ -3,10 +3,12 @@
 setlocal EnableDelayedExpansion
 set tree_sitter="%cd%\target\release\tree-sitter"
 
+IF "%~1" == "--build" set build="--build"
+
 for /f "tokens=*" %%f in ('dir test\fixtures\grammars\grammar.js /b/s') do (
   pushd "%%f\.."
   echo Regenerating parser !cd!
-  %tree_sitter% generate src\grammar.json --no-bindings --abi=latest
+  %tree_sitter% generate src\grammar.json --no-bindings --abi=latest !build!
   popd
 )
 
