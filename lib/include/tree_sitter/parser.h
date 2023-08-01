614f2f61c84049331cd4ec2b29c407ca338c2228
Amaan Qureshi <amaanq12@gmail.com>
2023-08-01 11:16:16 +0100

    fix: suppress unused warnings for eof

--- lib/include/tree_sitter/parser.h
+++ lib/include/tree_sitter/parser.h
@@ -130,9 +130,16 @@ struct TSLanguage {
  *  Lexer Macros
  */
 
+#ifdef _MSC_VER
+#define UNUSED __pragma(warning(suppress : 4101))
+#else
+#define UNUSED __attribute__((unused))
+#endif
+
 #define START_LEXER()           \
   bool result = false;          \
   bool skip = false;            \
+  UNUSED                        \
   bool eof = false;             \
   int32_t lookahead;            \
   goto start;                   \
