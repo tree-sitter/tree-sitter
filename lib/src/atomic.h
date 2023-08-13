cfd9c352031da1758c95c35d1ace4e071f26127a
Andrew Hlynskyi <ahlincq@gmail.com>
2023-08-13 17:49:27 +0300

    Revert "fix: musl compilation"

--- lib/src/atomic.h
+++ lib/src/atomic.h
@@ -1,7 +1,6 @@
 #ifndef TREE_SITTER_ATOMIC_H_
 #define TREE_SITTER_ATOMIC_H_
 
-#include <stddef.h>
 #include <stdint.h>
 
 #ifdef __TINYC__
@@ -47,19 +46,11 @@ static inline size_t atomic_load(const volatile size_t *p) {
 }
 
 static inline uint32_t atomic_inc(volatile uint32_t *p) {
-  #ifdef __ATOMIC_RELAXED
-    return __atomic_add_fetch(p, 1U, __ATOMIC_RELAXED);
-  #else
-    return __sync_add_and_fetch(p, 1U);
-  #endif
+  return __sync_add_and_fetch(p, 1U);
 }
 
 static inline uint32_t atomic_dec(volatile uint32_t *p) {
-  #ifdef __ATOMIC_RELAXED
-    return __atomic_sub_fetch(p, 1U, __ATOMIC_RELAXED);
-  #else
-    return __sync_sub_and_fetch(p, 1U);
-  #endif
+  return __sync_sub_and_fetch(p, 1U);
 }
 
 #endif
