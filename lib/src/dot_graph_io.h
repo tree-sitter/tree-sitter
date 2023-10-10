dde9d5c542a409a093525441dae95dbbd8dddf4c
Yehor Borkov <yehor.borkov@gmail.com>
2023-10-10 23:40:31 +0300

    fix: typo

--- lib/src/dot_graph_io.h
+++ lib/src/dot_graph_io.h
@@ -8,7 +8,7 @@ extern "C" {
 int ts_dot_graph_io_make_fd(int fd);
 int ts_dot_graph_io_make_fd_win(intptr_t osfhandle);
 FILE* ts_dot_graph_io_open(int fd);
-int ts_dot_graph_io_close(FILE* fie);
+int ts_dot_graph_io_close(FILE* file);
 
 #ifdef __cplusplus
 }
