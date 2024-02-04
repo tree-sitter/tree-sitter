1fb16a72ac1019178a41c93e1ab241f2f642b031
Amaan Qureshi <amaanq12@gmail.com>
2024-02-03 19:38:37 -0500

    chore(loader): apply clippy fixes

--- cli/loader/build.rs
+++ cli/loader/build.rs
@@ -5,8 +5,5 @@ fn main() {
     );
 
     let emscripten_version = std::fs::read_to_string("emscripten-version").unwrap();
-    println!(
-        "cargo:rustc-env={}={}",
-        "EMSCRIPTEN_VERSION", emscripten_version,
-    );
+    println!("cargo:rustc-env=EMSCRIPTEN_VERSION={emscripten_version}");
 }
