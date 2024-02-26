f11ee6424a97d08a83ece9900d6a3abcdc3223de
Segev Finer <segev208@gmail.com>
2024-02-26 20:01:23 +0200

    Update cli/src/generate/templates/js-binding.cc
    Co-authored-by: Boris Verkhovskiy <boris.verk@gmail.com>

--- cli/src/generate/templates/js-binding.cc
+++ cli/src/generate/templates/js-binding.cc
@@ -1,7 +1,7 @@
 #include "tree_sitter/parser.h"
 #include <napi.h>
 
-extern "C" TSLanguage * tree_sitter_PARSER_NAME();
+extern "C" TSLanguage *tree_sitter_PARSER_NAME();
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
   exports["name"] = Napi::String::New(env, "PARSER_NAME");
