1d043487d31accadda0173da69ec55943352aeaf
Segev Finer <segev@swimm.io>
2024-02-26 16:20:03 +0200

    Update binding.cc

--- cli/src/generate/templates/binding.cc
+++ cli/src/generate/templates/binding.cc
@@ -1,18 +1,16 @@
 #include "tree_sitter/parser.h"
 #include <napi.h>
 
-using namespace Napi;
-
 extern "C" TSLanguage * tree_sitter_PARSER_NAME();
 
 namespace {
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports["name"] = String::New(env, "PARSER_NAME");
-  exports["language"] = External<TSLanguage>::New(env, tree_sitter_PARSER_NAME());
+  exports["name"] = Napi::String::New(env, "PARSER_NAME");
+  exports["language"] = Napi::External<TSLanguage>::New(env, tree_sitter_PARSER_NAME());
   return exports;
 }
 
-NODE_API_MODULE(tree_sitter_PARSER_NAME_binding, Init)
-
 }  // namespace
+
+NODE_API_MODULE(tree_sitter_PARSER_NAME_binding, Init)
