21f7894a9dae9e073b7890d1be5c11b87330f7b5
Amaan Qureshi <amaanq12@gmail.com>
2024-02-27 16:14:08 -0500

    fixup!: replace existing node bindings
    Co-authored-by: ObserverOfTime <chronobserver@disroot.org>

--- cli/src/generate/templates/js-binding.cc
+++ cli/src/generate/templates/js-binding.cc
@@ -1,12 +1,13 @@
-#include "tree_sitter/parser.h"
 #include <napi.h>
+
+typedef struct TSLanguage TSLanguage;
 
 extern "C" TSLanguage *tree_sitter_PARSER_NAME();
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports["name"] = Napi::String::New(env, "PARSER_NAME");
-  exports["language"] = Napi::External<TSLanguage>::New(env, tree_sitter_PARSER_NAME());
-  return exports;
+    exports["name"] = Napi::String::New(env, "PARSER_NAME");
+    exports["language"] = Napi::External<TSLanguage>::New(env, tree_sitter_PARSER_NAME());
+    return exports;
 }
 
 NODE_API_MODULE(tree_sitter_PARSER_NAME_binding, Init)
