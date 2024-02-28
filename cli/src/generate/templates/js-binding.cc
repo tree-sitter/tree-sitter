875cc674041d6ba499f5dda63a86d9dee645c621
Segev Finer <segev@swimm.io>
2024-02-28 23:59:59 +0200

    Type tag the language in the node bindings

--- cli/src/generate/templates/js-binding.cc
+++ cli/src/generate/templates/js-binding.cc
@@ -4,9 +4,15 @@ typedef struct TSLanguage TSLanguage;
 
 extern "C" TSLanguage *tree_sitter_PARSER_NAME();
 
+const napi_type_tag LANGUAGE_TYPE_TAG = {
+  0x95840BEBF71E4E90, 0x9DC9419B874C0271
+};
+
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
     exports["name"] = Napi::String::New(env, "PARSER_NAME");
-    exports["language"] = Napi::External<TSLanguage>::New(env, tree_sitter_PARSER_NAME());
+    auto language = Napi::External<TSLanguage>::New(env, tree_sitter_PARSER_NAME());
+    language.TypeTag(&LANGUAGE_TYPE_TAG);
+    exports["language"] = language;
     return exports;
 }
 
