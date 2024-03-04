9bd1ae5fe7c9dfb8e9091ac5fc1d0c395e157994
ObserverOfTime <chronobserver@disroot.org>
2024-03-04 14:35:57 +0200

    feat(bindings): add query constants to python

--- cli/src/generate/templates/__init__.py
+++ cli/src/generate/templates/__init__.py
@@ -1,5 +1,20 @@
-"CAMEL_PARSER_NAME grammar for tree-sitter"
+"""CAMEL_PARSER_NAME grammar for tree-sitter"""
+
+from importlib.resources import files as _files
 
 from ._binding import language
 
-__all__ = ["language"]
+# NOTE: uncomment these to include any queries that this grammar contains:
+
+# HIGHLIGHTS_QUERY = _files(f"{__package__}.queries").joinpath("highlights.scm").read_text()
+# INJECTIONS_QUERY = _files(f"{__package__}.queries").joinpath("injections.scm").read_text()
+# LOCALS_QUERY = _files(f"{__package__}.queries").joinpath("locals.scm").read_text()
+# TAGS_QUERY = _files(f"{__package__}.queries").joinpath("tags.scm").read_text()
+
+__all__ = [
+    "language",
+    # "HIGHLIGHTS_QUERY",
+    # "INJECTIONS_QUERY",
+    # "LOCALS_QUERY",
+    # "TAGS_QUERY",
+]
