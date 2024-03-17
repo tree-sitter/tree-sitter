754aa62e894b1a24fc1bc77465df9a2bf5e4a36e
Amaan Qureshi <amaanq12@gmail.com>
2024-03-16 20:11:07 -0400

    chore: turbofish styling

--- cli/src/generate/parse_grammar.rs
+++ cli/src/generate/parse_grammar.rs
@@ -92,7 +92,7 @@ pub(crate) struct GrammarJSON {
 }
 
 pub(crate) fn parse_grammar(input: &str) -> Result<InputGrammar> {
-    let grammar_json: GrammarJSON = serde_json::from_str(input)?;
+    let grammar_json = serde_json::from_str::<GrammarJSON>(input)?;
 
     let mut variables = Vec::with_capacity(grammar_json.rules.len());
     for (name, value) in grammar_json.rules {
