646a2cc269b8d9676d05de10f592606d546b338f
ObserverOfTime <chronobserver@disroot.org>
2024-05-07 15:24:15 +0300

    fixup! lazy-load queries

--- cli/src/generate/templates/__init__.py
+++ cli/src/generate/templates/__init__.py
@@ -4,12 +4,27 @@ from importlib.resources import files as _files
 
 from ._binding import language
 
-# NOTE: uncomment these to include any queries that this grammar contains:
 
-# HIGHLIGHTS_QUERY = _files(f"{__package__}.queries").joinpath("highlights.scm").read_text()
-# INJECTIONS_QUERY = _files(f"{__package__}.queries").joinpath("injections.scm").read_text()
-# LOCALS_QUERY = _files(f"{__package__}.queries").joinpath("locals.scm").read_text()
-# TAGS_QUERY = _files(f"{__package__}.queries").joinpath("tags.scm").read_text()
+def _get_query(name, file):
+    query = _files(f"{__package__}.queries") / file
+    globals()[name] = query.read_text()
+    return globals()[name]
+
+
+def __getattr__(name):
+    # NOTE: uncomment these to include any queries that this grammar contains:
+
+    # if name == "HIGHLIGHTS_QUERY":
+    #     return _get_query("HIGHLIGHTS_QUERY", "highlights.scm")
+    # if name == "INJECTIONS_QUERY":
+    #     return _get_query("INJECTIONS_QUERY", "injections.scm")
+    # if name == "LOCALS_QUERY":
+    #     return _get_query("LOCALS_QUERY", "locals.scm")
+    # if name == "TAGS_QUERY":
+    #     return _get_query("TAGS_QUERY", "tags.scm")
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
 
 __all__ = [
     "language",
@@ -18,3 +33,10 @@ __all__ = [
     # "LOCALS_QUERY",
     # "TAGS_QUERY",
 ]
+
+
+def __dir__():
+    return sorted(__all__ + [
+        "__all__", "__builtins__", "__cached__", "__doc__", "__file__",
+        "__loader__", "__name__", "__package__", "__path__", "__spec__",
+    ])
