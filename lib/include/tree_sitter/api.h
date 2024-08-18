3c7c17b00bde2aebb5bc0d8f96ae444e42b8fc07
Matt Guerrette <direct3Dtutorials@gmail.com>
2024-08-18 05:01:13 -0400

    fix(lib): fix api header C++ interop (#3534)

--- lib/include/tree_sitter/api.h
+++ lib/include/tree_sitter/api.h
@@ -7,13 +7,13 @@
 #endif
 #endif
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /****************************/
 /* Section - ABI Versioning */
