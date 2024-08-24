2150c91114c8377329413752439bce5e59383519
Amaan Qureshi <amaanq12@gmail.com>
2024-08-23 23:27:28 -0400

    fix(bindings): update go bindings

--- cli/src/generate/templates/binding_test.go
+++ cli/src/generate/templates/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_PARSER_NAME_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-PARSER_NAME"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_PARSER_NAME "github.com/tree-sitter/tree-sitter-PARSER_NAME/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
