0988bb71b206b2da3f2aee7058a784ed6d8b393a
Amaan Qureshi <amaanq12@gmail.com>
2024-09-27 13:22:00 -0400

    style(lib): add parameter names in declarations that are missing them

--- lib/src/tree_cursor.h
+++ lib/src/tree_cursor.h
@@ -23,19 +23,19 @@ typedef enum {
   TreeCursorStepVisible,
 } TreeCursorStep;
 
-void ts_tree_cursor_init(TreeCursor *, TSNode);
+void ts_tree_cursor_init(TreeCursor *self, TSNode node);
 void ts_tree_cursor_current_status(
-  const TSTreeCursor *,
-  TSFieldId *,
-  bool *,
-  bool *,
-  bool *,
-  TSSymbol *,
-  unsigned *
+  const TSTreeCursor *_self,
+  TSFieldId *field_id,
+  bool *has_later_siblings,
+  bool *has_later_named_siblings,
+  bool *can_have_later_siblings_with_this_field,
+  TSSymbol *supertypes,
+  unsigned *supertype_count
 );
 
-TreeCursorStep ts_tree_cursor_goto_first_child_internal(TSTreeCursor *);
-TreeCursorStep ts_tree_cursor_goto_next_sibling_internal(TSTreeCursor *);
+TreeCursorStep ts_tree_cursor_goto_first_child_internal(TSTreeCursor *_self);
+TreeCursorStep ts_tree_cursor_goto_next_sibling_internal(TSTreeCursor *_self);
 
 static inline Subtree ts_tree_cursor_current_subtree(const TSTreeCursor *_self) {
   const TreeCursor *self = (const TreeCursor *)_self;
@@ -43,6 +43,6 @@ static inline Subtree ts_tree_cursor_current_subtree(const TSTreeCursor *_self) 
   return *last_entry->subtree;
 }
 
-TSNode ts_tree_cursor_parent_node(const TSTreeCursor *);
+TSNode ts_tree_cursor_parent_node(const TSTreeCursor *_self);
 
 #endif  // TREE_SITTER_TREE_CURSOR_H_
--- lib/src/wasm_store.h
+++ lib/src/wasm_store.h
@@ -8,21 +8,21 @@ extern "C" {
 #include "tree_sitter/api.h"
 #include "./parser.h"
 
-bool ts_wasm_store_start(TSWasmStore *, TSLexer *, const TSLanguage *);
-void ts_wasm_store_reset(TSWasmStore *);
-bool ts_wasm_store_has_error(const TSWasmStore *);
+bool ts_wasm_store_start(TSWasmStore *self, TSLexer *lexer, const TSLanguage *language);
+void ts_wasm_store_reset(TSWasmStore *self);
+bool ts_wasm_store_has_error(const TSWasmStore *self);
 
-bool ts_wasm_store_call_lex_main(TSWasmStore *, TSStateId);
-bool ts_wasm_store_call_lex_keyword(TSWasmStore *, TSStateId);
+bool ts_wasm_store_call_lex_main(TSWasmStore *self, TSStateId state);
+bool ts_wasm_store_call_lex_keyword(TSWasmStore *self, TSStateId state);
 
-uint32_t ts_wasm_store_call_scanner_create(TSWasmStore *);
-void ts_wasm_store_call_scanner_destroy(TSWasmStore *, uint32_t);
-bool ts_wasm_store_call_scanner_scan(TSWasmStore *, uint32_t, uint32_t);
-uint32_t ts_wasm_store_call_scanner_serialize(TSWasmStore *, uint32_t, char *);
-void ts_wasm_store_call_scanner_deserialize(TSWasmStore *, uint32_t, const char *, unsigned);
+uint32_t ts_wasm_store_call_scanner_create(TSWasmStore *self);
+void ts_wasm_store_call_scanner_destroy(TSWasmStore *self, uint32_t scanner_address);
+bool ts_wasm_store_call_scanner_scan(TSWasmStore *self, uint32_t scanner_address, uint32_t valid_tokens_ix);
+uint32_t ts_wasm_store_call_scanner_serialize(TSWasmStore *self, uint32_t scanner_address, char *buffer);
+void ts_wasm_store_call_scanner_deserialize(TSWasmStore *self, uint32_t scanner, const char *buffer, unsigned length);
 
-void ts_wasm_language_retain(const TSLanguage *);
-void ts_wasm_language_release(const TSLanguage *);
+void ts_wasm_language_retain(const TSLanguage *self);
+void ts_wasm_language_release(const TSLanguage *self);
 
 #ifdef __cplusplus
 }
