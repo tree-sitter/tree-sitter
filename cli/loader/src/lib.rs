00f9a71aad0824d967c18ec95f2ba15122f91d83
Amaan Qureshi <amaanq12@gmail.com>
2024-10-05 00:56:02 -0400

    chore(cli): minor correction in comments
    (cherry picked from commit c611e15a310d9af2ed18172ef1faafaf18506176)

--- cli/loader/src/lib.rs
+++ cli/loader/src/lib.rs
@@ -1118,8 +1118,8 @@ impl Loader {
             let language_count = self.languages_by_id.len();
             for grammar in config.grammars {
                 // Determine the path to the parser directory. This can be specified in
-                // the package.json, but defaults to the directory containing the
-                // package.json.
+                // the tree-sitter.json, but defaults to the directory containing the
+                // tree-sitter.json.
                 let language_path = parser_path.join(grammar.path);
 
                 // Determine if a previous language configuration in this package.json file
