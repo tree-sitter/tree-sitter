d25a5e488673f1e53293c225251895211f43e8b3
Amaan Qureshi <amaanq12@gmail.com>
2024-11-09 21:46:33 -0500

    feat(node): support single-file executables via bun build --compile
    (cherry picked from commit 134233f33abdf80d4d55eebf91a1c1e11d02a50f)

--- cli/src/templates/index.js
+++ cli/src/templates/index.js
@@ -1,6 +1,10 @@
 const root = require("path").join(__dirname, "..", "..");
 
-module.exports = require("node-gyp-build")(root);
+module.exports =
+  typeof process.versions.bun === "string"
+    // Support `bun build --compile` by being statically analyzable enough to find the .node file at build-time
+    ? require(`../../prebuilds/${process.platform}-${process.arch}/tree-sitter-typescript.node`)
+    : require("node-gyp-build")(root);
 
 try {
   module.exports.nodeTypeInfo = require("../../src/node-types.json");
