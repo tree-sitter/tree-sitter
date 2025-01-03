c8a1f6b4d213c6543542ca797c23d9b5723a4530
Amaan Qureshi <amaanq12@gmail.com>
2025-01-03 15:04:15 -0500

    fix: update outdated links

--- cli/src/templates/lib.rs
+++ cli/src/templates/lib.rs
@@ -31,7 +31,7 @@ pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_PARSE
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
-/// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
+/// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers/6-static-node-types
 pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 // NOTE: uncomment these to include any queries that this grammar contains:
