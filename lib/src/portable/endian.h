aea3a4720a3aecd033d357d79a3fbf28ba32fdc5
Lucas Marçal <lucas.marcal@faire.com>
2025-01-05 20:41:15 -0300

    fix(endian): support POSIX mode on Apple platforms

--- lib/src/portable/endian.h
+++ lib/src/portable/endian.h
@@ -18,29 +18,63 @@
 #    include <endian.h>
 
 #elif defined(__APPLE__)
-
-#    include <libkern/OSByteOrder.h>
-
-#    define htobe16(x) OSSwapHostToBigInt16(x)
-#    define htole16(x) OSSwapHostToLittleInt16(x)
-#    define be16toh(x) OSSwapBigToHostInt16(x)
-#    define le16toh(x) OSSwapLittleToHostInt16(x)
- 
-#    define htobe32(x) OSSwapHostToBigInt32(x)
-#    define htole32(x) OSSwapHostToLittleInt32(x)
-#    define be32toh(x) OSSwapBigToHostInt32(x)
-#    define le32toh(x) OSSwapLittleToHostInt32(x)
- 
-#    define htobe64(x) OSSwapHostToBigInt64(x)
-#    define htole64(x) OSSwapHostToLittleInt64(x)
-#    define be64toh(x) OSSwapBigToHostInt64(x)
-#    define le64toh(x) OSSwapLittleToHostInt64(x)
-
 #    define __BYTE_ORDER    BYTE_ORDER
 #    define __BIG_ENDIAN    BIG_ENDIAN
 #    define __LITTLE_ENDIAN LITTLE_ENDIAN
 #    define __PDP_ENDIAN    PDP_ENDIAN
 
+#    if !defined(_POSIX_C_SOURCE)
+#        include <libkern/OSByteOrder.h>
+
+#        define htobe16(x) OSSwapHostToBigInt16(x)
+#        define htole16(x) OSSwapHostToLittleInt16(x)
+#        define be16toh(x) OSSwapBigToHostInt16(x)
+#        define le16toh(x) OSSwapLittleToHostInt16(x)
+
+#        define htobe32(x) OSSwapHostToBigInt32(x)
+#        define htole32(x) OSSwapHostToLittleInt32(x)
+#        define be32toh(x) OSSwapBigToHostInt32(x)
+#        define le32toh(x) OSSwapLittleToHostInt32(x)
+
+#        define htobe64(x) OSSwapHostToBigInt64(x)
+#        define htole64(x) OSSwapHostToLittleInt64(x)
+#        define be64toh(x) OSSwapBigToHostInt64(x)
+#        define le64toh(x) OSSwapLittleToHostInt64(x)
+#    else
+#        if BYTE_ORDER == LITTLE_ENDIAN
+#            define htobe16(x) __builtin_bswap16(x)
+#            define htole16(x) (x)
+#            define be16toh(x) __builtin_bswap16(x)
+#            define le16toh(x) (x)
+
+#            define htobe32(x) __builtin_bswap32(x)
+#            define htole32(x) (x)
+#            define be32toh(x) __builtin_bswap32(x)
+#            define le32toh(x) (x)
+
+#            define htobe64(x) __builtin_bswap64(x)
+#            define htole64(x) (x)
+#            define be64toh(x) __builtin_bswap64(x)
+#            define le64toh(x) (x)
+#        elif BYTE_ORDER == BIG_ENDIAN
+#            define htobe16(x) (x)
+#            define htole16(x) __builtin_bswap16(x)
+#            define be16toh(x) (x)
+#            define le16toh(x) __builtin_bswap16(x)
+
+#            define htobe32(x) (x)
+#            define htole32(x) __builtin_bswap32(x)
+#            define be32toh(x) (x)
+#            define le32toh(x) __builtin_bswap32(x)
+
+#            define htobe64(x) (x)
+#            define htole64(x) __builtin_bswap64(x)
+#            define be64toh(x) (x)
+#            define le64toh(x) __builtin_bswap64(x)
+#        else
+#            error byte order not supported
+#        endif
+#    endif
 #elif defined(__OpenBSD__)
 
 #    include <endian.h>
