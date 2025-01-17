6e88672dacd68b4e41776bbd45d67ab1750318d5
Amaan Qureshi <amaanq12@gmail.com>
2025-01-17 11:44:27 -0500

    chore: cleanup unused code

--- lib/src/point.h
+++ lib/src/point.h
@@ -45,18 +45,4 @@ static inline bool point_eq(TSPoint a, TSPoint b) {
   return a.row == b.row && a.column == b.column;
 }
 
-static inline TSPoint point_min(TSPoint a, TSPoint b) {
-  if (a.row < b.row || (a.row == b.row && a.column < b.column))
-    return a;
-  else
-    return b;
-}
-
-static inline TSPoint point_max(TSPoint a, TSPoint b) {
-  if (a.row > b.row || (a.row == b.row && a.column > b.column))
-    return a;
-  else
-    return b;
-}
-
 #endif
