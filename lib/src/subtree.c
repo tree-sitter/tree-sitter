d028c0f14328ae5575a40faa2f286ca1dd69f011
Amaan Qureshi <amaanq12@gmail.com>
2025-04-28 18:17:47 -0400

    fix(lib): do not access the alias sequence for the `end` subtree in `ts_subtree_summarize_children`
    (cherry picked from commit 21c658a12c27d20651ac3eec7e8c03ebdd9258c2)

--- lib/src/subtree.c
+++ lib/src/subtree.c
@@ -407,7 +407,12 @@ void ts_subtree_summarize_children(
     self.ptr->dynamic_precedence += ts_subtree_dynamic_precedence(child);
     self.ptr->visible_descendant_count += ts_subtree_visible_descendant_count(child);
 
-    if (alias_sequence && alias_sequence[structural_index] != 0 && !ts_subtree_extra(child)) {
+    if (
+      !ts_subtree_extra(child) &&
+      ts_subtree_symbol(child) != 0 &&
+      alias_sequence &&
+      alias_sequence[structural_index] != 0
+    ) {
       self.ptr->visible_descendant_count++;
       self.ptr->visible_child_count++;
       if (ts_language_symbol_metadata(language, alias_sequence[structural_index]).named) {
