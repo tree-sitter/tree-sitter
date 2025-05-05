c7e26f10a528f998dcce405df20c5500e312547f
ObserverOfTime <chronobserver@disroot.org>
2025-05-05 12:08:57 +0300

    fix(bindings): update swift & node dependencies
    also add the dependency name to Package.swift

--- crates/cli/src/templates/package.swift
+++ crates/cli/src/templates/package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "PARSER_CLASS_NAME", targets: ["PARSER_CLASS_NAME"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.8.0"),
+        .package(name: "SwiftTreeSitter", url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.9.0"),
     ],
     targets: [
         .target(
