9dd25ffa4beb75acad1506d435dc4ac397d93554
Will Lillis <will.lillis24@gmail.com>
2025-07-27 00:12:37 -0400

    feat(cli): include language name in `dump-languages` output

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1761,6 +1761,7 @@ impl DumpLanguages {
         for (configuration, language_path) in loader.get_all_language_configurations() {
             println!(
                 concat!(
+                    "name: {}\n",
                     "scope: {}\n",
                     "parser: {:?}\n",
                     "highlights: {:?}\n",
@@ -1768,6 +1769,7 @@ impl DumpLanguages {
                     "content_regex: {:?}\n",
                     "injection_regex: {:?}\n",
                 ),
+                configuration.language_name,
                 configuration.scope.as_ref().unwrap_or(&String::new()),
                 language_path,
                 configuration.highlights_filenames,
