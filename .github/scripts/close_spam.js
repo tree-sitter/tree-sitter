c12b4a15654c6a40b4109b35c3035e46d8e6bf63
ObserverOfTime <chronobserver@disroot.org>
2025-08-16 23:06:57 +0300

    ci: add a spam closing workflow

---
+++ .github/scripts/close_spam.js
@@ -0,0 +1,38 @@
+module.exports = async ({ github, context }) => {
+  const { data } = await github.rest.repos.getCollaboratorPermissionLevel({
+    ...context.repo,
+    username: context.actor
+  });
+  if (!data.permission.includes("triage")) {
+    await github.log.error("Workflow called with insufficient permissions!");
+    return;
+  }
+
+  let target = context.payload.issue;
+  if (target) {
+    await github.rest.issues.update({
+      ...context.repo,
+      issue_number: target.number,
+      state: "closed",
+      state_reason: "not_planned",
+      title: "[spam]",
+      body: "",
+      type: null,
+    });
+  } else {
+    target = context.payload.pull_request;
+    await github.rest.pulls.update({
+      ...context.repo,
+      pull_number: target.number,
+      state: "closed",
+      title: "[spam]",
+      body: "",
+    });
+  }
+
+  await github.rest.issues.lock({
+    ...context.repo,
+    issue_number: target.number,
+    lock_reason: "spam",
+  });
+};
