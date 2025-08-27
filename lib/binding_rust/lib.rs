17cb10a67724dd19b36ef3be215e14701ecbb613
Quentin LE DILAVREC <ledilavrecquentin@gmail.com>
2025-08-27 10:25:29 +0200

    fix(rust): EqCapture accepted cases where number of captured nodes differed by one
    Problem: When using alternations, the `#eq?` predicate does not always use the same capture name.
    
    Solution: Iterate the left and right captured nodes more independently.
    (cherry picked from commit 79177a1cd526cedc2e08deed7b595bbdd17a30b8)

--- lib/binding_rust/lib.rs
+++ lib/binding_rust/lib.rs
@@ -3353,9 +3353,11 @@ impl<'tree> QueryMatch<'_, 'tree> {
             .iter()
             .all(|predicate| match predicate {
                 TextPredicateCapture::EqCapture(i, j, is_positive, match_all_nodes) => {
-                    let mut nodes_1 = self.nodes_for_capture_index(*i);
-                    let mut nodes_2 = self.nodes_for_capture_index(*j);
-                    while let (Some(node1), Some(node2)) = (nodes_1.next(), nodes_2.next()) {
+                    let mut nodes_1 = self.nodes_for_capture_index(*i).peekable();
+                    let mut nodes_2 = self.nodes_for_capture_index(*j).peekable();
+                    while nodes_1.peek().is_some() && nodes_2.peek().is_some() {
+                        let node1 = nodes_1.next().unwrap();
+                        let node2 = nodes_2.next().unwrap();
                         let mut text1 = text_provider.text(node1);
                         let mut text2 = text_provider.text(node2);
                         let text1 = node_text1.get_text(&mut text1);
