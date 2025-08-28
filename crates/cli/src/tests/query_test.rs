97e4b07c8c7cedd350c4a77a0f0886ec5ff53c07
Amaan Qureshi <contact@amaanq.com>
2025-08-28 17:10:58 -0400

    fix(lib): allow error nodes to match when they are child nodes

--- crates/cli/src/tests/query_test.rs
+++ crates/cli/src/tests/query_test.rs
@@ -5738,3 +5738,25 @@ fn test_query_with_anonymous_error_node() {
         vec![(1, vec![("error", "ERROR")]), (0, vec![("error", "ERROR")])]
     );
 }
+
+#[test]
+fn test_query_allows_error_nodes_with_children() {
+    allocations::record(|| {
+        let language = get_language("cpp");
+
+        let code = "SomeStruct foo{.bar{}};";
+
+        let mut parser = Parser::new();
+        parser.set_language(&language).unwrap();
+
+        let tree = parser.parse(code, None).unwrap();
+        let root = tree.root_node();
+
+        let query = Query::new(&language, "(initializer_list (ERROR) @error)").unwrap();
+        let mut cursor = QueryCursor::new();
+
+        let matches = cursor.matches(&query, root, code.as_bytes());
+        let matches = collect_matches(matches, &query, code);
+        assert_eq!(matches, &[(0, vec![("error", ".bar")])]);
+    });
+}
