046d5f17f37993091f7883e2b2e9f59f1132a48b
Amaan Qureshi <contact@amaanq.com>
2025-08-28 14:47:20 -0400

    fix(lib): check if an `ERROR` node is named before assuming it's the builtin error node

--- lib/src/language.c
+++ lib/src/language.c
@@ -186,7 +186,7 @@ TSSymbol ts_language_symbol_for_name(
   uint32_t length,
   bool is_named
 ) {
-  if (!strncmp(string, "ERROR", length)) return ts_builtin_sym_error;
+  if (is_named && !strncmp(string, "ERROR", length)) return ts_builtin_sym_error;
   uint16_t count = (uint16_t)ts_language_symbol_count(self);
   for (TSSymbol i = 0; i < count; i++) {
     TSSymbolMetadata metadata = ts_language_symbol_metadata(self, i);
