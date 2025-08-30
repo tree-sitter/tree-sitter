31c824a2451daa5fb352a9d8ea3a6fae87b84e79
ObserverOfTime <chronobserver@disroot.org>
2025-08-31 00:41:29 +0300

    refactor(web): use nullish coalescing operator

--- lib/binding_web/src/bindings.ts
+++ lib/binding_web/src/bindings.ts
@@ -10,10 +10,7 @@ export let Module: MainModule | null = null;
  * Initialize the Tree-sitter Wasm module. This should only be called by the {@link Parser} class via {@link Parser.init}.
  */
 export async function initializeBinding(moduleOptions?: Partial<EmscriptenModule>): Promise<MainModule> {
-  if (!Module) {
-    Module = await createModule(moduleOptions);
-  }
-  return Module;
+  return Module ??= await createModule(moduleOptions);
 }
 
 /**
