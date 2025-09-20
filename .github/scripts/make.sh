3ac5383b9df3feae7af34e24597e6c26f793684b
ObserverOfTime <chronobserver@disroot.org>
2025-09-20 09:38:54 +0300

    ci: cross-compile without cross-rs

--- .github/scripts/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eu
-
-tree_sitter="$ROOT"/target/"$TARGET"/release/tree-sitter
-
-if [[ $BUILD_CMD == cross ]]; then
-  cross.sh make CC="$CC" AR="$AR" "$@"
-else
-  exec make "$@"
-fi
--- .github/scripts/tree-sitter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eu
-
-tree_sitter="$ROOT"/target/"$TARGET"/release/tree-sitter
-
-if [[ $BUILD_CMD == cross ]]; then
-  cross.sh "$CROSS_RUNNER" "$tree_sitter" "$@"
-else
-  exec "$tree_sitter" "$@"
-fi
