9b914885f1174b03cba72fe45787c290caf94ee6
Max Brunsfeld <maxbrunsfeld@gmail.com>
2025-09-22 09:24:30 -0700

    Fix issues preventing releases from successfully publishing (#4867)
    * Correct the path to the CLI npm package in release job
    
    * Specify a version for tree-sitter-language
    
    * Fix path to README in doc include

--- lib/binding_rust/lib.rs
+++ lib/binding_rust/lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(not(any(test, doctest)), doc = include_str!("../README.md"))]
+#![cfg_attr(not(any(test, doctest)), doc = include_str!("./README.md"))]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
