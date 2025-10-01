ff255a2354fabe8ec36a3a936e70cf9b73f05126
WillLillis <wlillis@umass.edu>
2025-10-01 04:34:54 -0400

    test: add coverage for new test aggregation method

--- crates/cli/src/tests.rs
+++ crates/cli/src/tests.rs
@@ -26,6 +26,8 @@ pub use crate::fuzz::{
     ITERATION_COUNT,
 };
 
+pub use helpers::fixtures::get_language;
+
 /// This is a simple wrapper around [`tree_sitter_generate::generate_parser_for_grammar`], because
 /// our tests do not need to pass in a version number, only the grammar JSON.
 fn generate_parser(grammar_json: &str) -> GenerateResult<(String, String)> {
