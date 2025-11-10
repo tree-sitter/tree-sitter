1928e706865bcc93f0425cd40fa9fb3ea8b70ad9
takumi-earth <165885386+takumi-earth@users.noreply.github.com>
2025-11-10 19:49:50 +0800

    Return NULL when parse balancing fails

--- lib/src/parser.c
+++ lib/src/parser.c
@@ -2214,7 +2214,7 @@ balance:
   ts_assert(self->finished_tree.ptr);
   if (!ts_parser__balance_subtree(self)) {
     self->canceled_balancing = true;
-    return false;
+    return NULL;
   }
   self->canceled_balancing = false;
   LOG("done");
