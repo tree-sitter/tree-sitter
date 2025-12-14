e08716744d03428105c11e5d57e1fa640541dbc3
Christian Clason <c.clason@uni-graz.at>
2025-12-14 16:43:25 +0100

    fix(wasm): add `iswpunct` to stdlib

--- crates/language/wasm/src/wctype.c
+++ crates/language/wasm/src/wctype.c
@@ -7,3 +7,10 @@ int iswlower(wint_t wch) {
 int iswupper(wint_t wch) {
     return (unsigned)wch - L'A' < 26;
 }
+
+int iswpunct(wint_t wch) {
+  return (wch >= 33 && wch <= 47) ||
+         (wch >= 58 && wch <= 64) ||
+         (wch >= 91 && wch <= 96) ||
+         (wch >= 123 && wch <= 126);
+}
