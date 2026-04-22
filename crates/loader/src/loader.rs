8850e11bd818df15c1318942a156efb3444b2aaf
Volker Mische <volker.mische@gmail.com>
2026-04-22 09:41:04 +0200

    fix(loader): allow filenames with dots (#5529)
    (cherry picked from commit 4cb11acd46ce5c5cae7005c3e69ebcefad780745)

--- crates/loader/src/loader.rs
+++ crates/loader/src/loader.rs
@@ -810,8 +810,11 @@ impl Loader {
                     path = PathBuf::from(path.file_stem()?.to_os_string());
                 }
                 extensions.reverse();
-                self.language_configuration_ids_by_file_type
-                    .get(&extensions.join("."))
+                // Try longest extension suffixs first (e.g. "foo.bar.baz"->"bar.baz"->"baz"),
+                // stopping at the first match.
+                (0..extensions.len())
+                    .map(|i| extensions[i..].join("."))
+                    .find_map(|key| self.language_configuration_ids_by_file_type.get(&key))
             });
 
         if let Some(configuration_ids) = configuration_ids {
