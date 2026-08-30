26e429b66016b10c63db7df4424ca16c9624cff0
Christian Clason <c.clason@uni-graz.at>
2026-08-30 19:31:48 +0200

    chore: start working on v0.28

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
       eachSystem = lib.genAttrs systems;
       pkgsFor = inputs.nixpkgs.legacyPackages;
 
-      version = "0.27.0";
+          version = "0.28.0";
 
       fs = lib.fileset;
       src = fs.toSource {
